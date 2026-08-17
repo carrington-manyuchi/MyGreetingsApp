f985b1088a5a33d32af49b638b0422522c7e1099
Carrington Manyuchi <carrington.manyuchi@standardbank.co.za>
2026-08-17 19:48:54 +0200

    Initial Commit

---
+++ MyGreetingsApp/MyGreetingsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyGreetingsAppApp.swift
+//  MyGreetingsApp
+//
+//  Created by Manyuchi, Carrington C on 2026/08/17.
+//
+
+import SwiftUI
+
+@main
+struct MyGreetingsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
